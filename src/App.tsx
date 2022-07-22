21f519c3b980629b23b27db7131ad1b796d0af56
Mary Meier <mrose041102@gmail.com>
2022-07-22 16:27:15 -0400

    added a navigation folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter as Switch, Router, Route, Routes } from "react-router-dom";
-import About from "./components/AboutPage";
-import Home from "./components/HomePage";
-import NavBar from "./components/NavBar";
-import NotFound from "./components/NotFoundPage";
+import { BrowserRouter as Router, Route, Routes, Switch, } from "react-router-dom";
+import About from "./components/navigation bar/AboutPage";
+import Home from "./components/navigation bar/HomePage";
+import NavBar from "./components/navigation bar/NavBar";
+import NotFound from "./components/navigation bar/NotFoundPage";
 import DataBase from "./database";
-// import "App.css";
+import "./components/styles/App.scss";
 
 function App() {
   return (
